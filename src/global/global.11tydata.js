7c373527faf976ea8afbafd2bad1f73943fff14e
Ryan Gittings <ryan@madebykingfisher.co.uk>
2020-11-25 20:26:49 +0000

    Fix language bugs

--- src/global/global.11tydata.js
+++ src/global/global.11tydata.js
@@ -2,11 +2,11 @@ const locales = require('../_data/locales');
 
 module.exports = {
   pagination: {
-    data: 'locales',
+    data: 'languages',
     size: 1,
     alias: 'locale',
   },
-  locales: locales.languages.map((lang) => lang.root),
+  languages: locales.languages.map((lang) => lang.root),
   permalink: '{{ locale }}{{ page.filePathStem | global | replace("index", "") }}/index.html',
   eleventyComputed: {
     locale: (data) => {
